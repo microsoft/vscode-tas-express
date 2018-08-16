5f60ed17be0cf242c1a71bcf16d88d3c20d5b268
Faiz Shaikh <fashaikh@microsoft.com>
2018-08-16 16:57:02 -0700

    read from metadata.json if it gets udpated

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const http = require('http');
 const fs = require('fs');
-const INDEX_CONTENT = fs
+var INDEX_CONTENT = fs
   .readFileSync(`${__dirname}/template.html`, 'utf8')
   .replace(
     '{ envVars }',
@@ -10,12 +10,29 @@ const INDEX_CONTENT = fs
       JSON.stringify({
         gitUrl: process.env['APPSETTING_SITE_GIT_URL'],
         bashGitUrl: process.env['APPSETTING_SITE_BASH_GIT_URL'],
-        expiry:JSON.parse(fs.readFileSync(`metadata.json`, 'utf8')).expiryTime,
+        expiry:JSON.parse(fs.readFileSync('metadata.json', 'utf8')).expiryTime,
         host: process.env['HTTP_HOST']
       })
   );
 // Azure App Service will set process.env.port for you, but we use 3000 in development.
 const PORT = process.env.PORT || 3000;
+
+fs.watchFile('metadata.json', (curr, prev) => {
+  console.log(`the current mtime is: ${curr.mtime}`);
+  INDEX_CONTENT = fs
+  .readFileSync(`${__dirname}/template.html`, 'utf8')
+  .replace(
+    '{ envVars }',
+    `let env = ` +
+      JSON.stringify({
+        gitUrl: process.env['APPSETTING_SITE_GIT_URL'],
+        bashGitUrl: process.env['APPSETTING_SITE_BASH_GIT_URL'],
+        expiry:JSON.parse(fs.readFileSync('metadata.json', 'utf8')).expiryTime,
+        host: process.env['HTTP_HOST']
+      })
+  )
+  console.log(`the previous mtime was: ${prev.mtime}`);
+});
 
 // Create the express routes
 let app = express();
