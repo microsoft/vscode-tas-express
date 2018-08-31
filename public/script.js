9f11e2c9fb7bf57e447c1a4812e555ab74abb346
chrisdias <cdias@microsoft.com>
2018-08-31 15:34:19 -0700

    Updating instructions

--- public/script.js
+++ public/script.js
@@ -18,7 +18,7 @@ function timeTo(ts) {
 
 function timeToString(ts) {
   const rec = timeTo(ts);
-  return `${rec.minutes} min ${rec.seconds} sec`;
+  return `${rec.minutes}`; // minutes`; // ${rec.seconds} sec`;
 }
 
 setInterval(() => {
@@ -28,20 +28,20 @@ setInterval(() => {
 
 document.addEventListener('DOMContentLoaded', () => {
   // set up the clone URLs.
-  const clone = document.getElementById('clone');
-  clone.setAttribute(
-    'href',
-    'vscode://vscode.git/clone?url=' + encodeURIComponent(env.gitUrl)
-  );
+  // const clone = document.getElementById('clone');
+  // clone.setAttribute(
+  //   'href',
+  //   'vscode://vscode.git/clone?url=' + encodeURIComponent(env.gitUrl)
+  // );
 
-  const cloneInsiders = document.getElementById('clone-insiders');
-  cloneInsiders.setAttribute(
-    'href',
-    'vscode-insiders://vscode.git/clone?url=' + encodeURIComponent(env.gitUrl)
-  );
+  // const cloneInsiders = document.getElementById('clone-insiders');
+  // cloneInsiders.setAttribute(
+  //   'href',
+  //   'vscode-insiders://vscode.git/clone?url=' + encodeURIComponent(env.gitUrl)
+  // );
 
-  const host = document.getElementById('host');
-  host.textContent = document.location.href;
+  // const host = document.getElementById('host');
+  // host.textContent = document.location.href;
 
   const creds = document.getElementById('creds');
   if (navigator.platform === 'Win32') {
