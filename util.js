92fb4d6d4e527d5300e9ec1e1c4ba21461e9c287
Faiz Shaikh <erfaiz@gmail.com>
2018-12-10 22:06:06 -0800

    use secure TAS API

--- util.js
+++ util.js
@@ -1,9 +1,5 @@
 //@ts-check
 const fs = require('fs');
-const util = require('util');
-const metaDataFileName = `${__dirname}/metadata.json`;
-const promisifiedReadFile = util.promisify(fs.readFile);
-
 module.exports.loadEnvironmentVariables = async function(updateObject) {    
     return await fs.readFileSync(`${__dirname}/template.html`, 'utf8')
         .replace(
@@ -11,16 +7,4 @@ module.exports.loadEnvironmentVariables = async function(updateObject) {
             `let env = ` +
             JSON.stringify(updateObject)
         );
-}
-
-module.exports.updateMetadataFile = function(expiryTimestamp) {
-    var file =  {expiryTimestamp : expiryTimestamp};
-    fs.writeFileSync(metaDataFileName, JSON.stringify(file));
-}
-
-module.exports.readMetadataFile = async function() {
-    if (fs.existsSync(metaDataFileName)) {
-        let content = await promisifiedReadFile(metaDataFileName, 'utf8');
-        return JSON.parse(content);
-    }
 }
